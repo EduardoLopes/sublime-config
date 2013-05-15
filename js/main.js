35f584a69cc5dc51d904729e8a5db0c75ac2b1a2
Eduardo Lopes <EduardoLopesDS@gmail.com>
2013-05-15 20:38:21 -0300

    Template and simple javascript.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
+function Config(){
+    this.config = {};
+}
 
+Config.prototype.setConfig = function(what, arg) {
+    this.config[what] = arg;
+    //console.log(this.config);
+};
+
+Config.prototype.getConfig = function(config) {
+    return this.config;
+};
+
+Config.prototype.removeConfig = function(what) {
+    delete this.config[what];
+};
+
+Config.prototype.getJson = function() {
+    var json = JSON.stringify(c.getConfig(), null, '\t');
+    return json.replace(/\"([^(\")"]+)\":/g,"$1:");
+};
+
+c = new Config();
+c.setConfig('teste1', true);
+c.setConfig('teste2', 10);
+c.setConfig('teste3', '3');
+c.setConfig('teste4', '4');
+c.setConfig('teste5', '5');
+c.setConfig('teste6', '6');
+
+//console.log(c.getJson());
