9865a6e475edead7e2ea47d4f415f49a4b08e9d9
Eduardo Lopes <EduardoLopesDS@gmail.com>
2013-10-03 15:41:15 -0300

    Remove 'app/views/main.html', everithing is inside of index.html
    - fix a issue with selects
    - remove outline of the selects

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,12 +2,5 @@
 
 angular.module('sublConfigApp', [])
   .config(['$routeProvider', function ($routeProvider) {
-      $routeProvider
-        .when('/', {
-          templateUrl: 'views/main.html',
-          controller: 'MainCtrl'
-        })
-        .otherwise({
-          redirectTo: '/'
-        });
+
     }]);
