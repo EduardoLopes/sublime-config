4337c65d6c3226dc14effcb0631d7de3e6dce5a7
Eduardo Lopes <EduardoLopesDS@gmail.com>
2013-10-04 03:45:59 -0300

    Add tests to main controller

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -10,13 +10,63 @@ describe('Controller: MainCtrl', function () {
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
+    scope = $rootScope;
     MainCtrl = $controller('MainCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should set a new config', function () {
+
+    scope.setConfig('spell_check', true);
+    expect(scope.config.spell_check).toBe(true);
   });
+
+  it('should remove a config', function () {
+
+    scope.setConfig('spell_check', true);
+    scope.removeConfig('spell_check');
+
+    expect(scope.config.spell_check).toBeFalsy(angular.isDefined(scope.config.spell_check));
+  });
+
+  it('should remove a config if the value is "default"', function () {
+
+    scope.setConfig('spell_check', true);
+    scope.setConfig('spell_check', 'default');
+
+    expect(scope.config.spell_check).toBeFalsy(angular.isDefined(scope.config.spell_check));
+  });
+
+  it('should set a new array options', function () {
+
+    scope.setArrayOptions('font_options', 'bold');
+    scope.setArrayOptions('font_options', 'italic');
+    scope.setArrayOptions('font_options', 'gray_antialias');
+
+    expect(scope.config.font_options).toContain('bold');
+    expect(scope.config.font_options).toContain('italic');
+    expect(scope.config.font_options).toContain('gray_antialias');
+  });
+
+  it('should set a new array options', function () {
+
+    scope.setArrayOptions('font_options', 'bold');
+    scope.setArrayOptions('font_options', 'italic');
+    scope.setArrayOptions('font_options', 'gray_antialias');
+    scope.removeArrayOptions('font_options', 'gray_antialias');
+
+    expect(scope.config.font_options).toContain('bold');
+    expect(scope.config.font_options).toContain('italic');
+    expect(scope.config.font_options).toEqual([ 'bold', 'italic']);
+  });
+
+  it('should remove the config if remove the last option', function () {
+
+    scope.setArrayOptions('font_options', 'bold');
+    scope.removeArrayOptions('font_options', 'bold');
+
+    expect(scope.config.font_options).toBeFalsy(angular.isDefined(scope.config.font_options));
+  });
+
 });
